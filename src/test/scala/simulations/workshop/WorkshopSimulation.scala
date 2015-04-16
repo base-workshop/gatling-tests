1ea3a576da8ff5b96738191dc239054036bbc910
Tomasz Nowak <tomasz.nowak@getbase.com>
2015-04-16 19:22:01 +0200

    Add post for website resource

--- src/test/scala/simulations/workshop/WorkshopSimulation.scala
+++ src/test/scala/simulations/workshop/WorkshopSimulation.scala
@@ -8,15 +8,42 @@ import simulations.Common._
 
 class WorkshopSimulation extends Simulation {
 
+  val httpConf = http
+//    .proxy(Proxy("127.0.0.1", 8888).httpsPort(8888))
+    .baseURL("https://workshop.uxguards.com/whipping-boy/api")
+    .disableCaching
+
+  val commonHeaders = Map(
+    "User-Agent" -> "gatling/simulation",
+    HttpHeaderNames.Accept -> HttpHeaderValues.ApplicationJson
+  )
+
+  val data = csv("data.csv").circular
+
+  def website(): String = {
+    """{"url":"${url}"}"""
+  }
+
+  val createWebsite = exec(
+    http("Create Website")
+      .post("/websites")
+      .body(StringBody(website()))
+      .asJSON
+      .check(status.is(201))
+  )
+
   val singleUserScenario = scenario("Single user")
+    .feed(data)
     .forever {
-      pause(1)
+      feed(data)
+      .pause(1)
       .exec(http("discovery").options("/").headers(commonHeaders))
+      .exec(createWebsite)
   }
 
   setUp(
     singleUserScenario.inject(
-      rampUsers(maxUsers()) over (rampDurationSec() seconds)
+      rampUsers(1) over (2 seconds)
     )
   ).protocols(httpConf)
     .maxDuration(testDurationSec() seconds)
