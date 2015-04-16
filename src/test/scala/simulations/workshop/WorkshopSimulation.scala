c19f36e64844eedea091d48fc50fa92059a96b82
unknown <slaklu@PLKRAL026.ad.global>
2015-04-16 21:26:32 +0200

    Modified test

--- src/test/scala/simulations/workshop/WorkshopSimulation.scala
+++ src/test/scala/simulations/workshop/WorkshopSimulation.scala
@@ -9,43 +9,83 @@ import simulations.Common._
 class WorkshopSimulation extends Simulation {
 
   val httpConf = http
-//    .proxy(Proxy("127.0.0.1", 8888).httpsPort(8888))
+    //    .proxy(Proxy("127.0.0.1", 8888).httpsPort(8888))
     .baseURL("https://workshop.uxguards.com/whipping-boy/api")
     .disableCaching
 
   val commonHeaders = Map(
     "User-Agent" -> "gatling/simulation",
-    HttpHeaderNames.Accept -> HttpHeaderValues.ApplicationJson
+    HttpHeaderNames.Accept -> HttpHeaderValues.ApplicationJson,
+    HttpHeaderNames.ContentType -> HttpHeaderValues.ApplicationJson
   )
 
   val data = csv("data.csv").circular
 
-  def website(): String = {
-    """{"url":"${url}"}"""
+  def artist(): String = {
+    """{"name":"${name}"}"""
   }
 
-  val createWebsite = exec(
+  def newArtist(): String = {
+    """{"name":"${name}-xxx"}"""
+  }
+
+  val createArtist = exec(
     http("Create Website")
-      .post("/websites")
-      .body(StringBody(website()))
+      .post("/artists")
+      .body(StringBody(artist()))
+      .asJSON
+      .check(status.is(201))
+      .check(jsonPath("$.id").saveAs("artist_id"))
+  )
+
+  val updateArtist = exec(
+    http("Update artist")
+      .put("/artists/${artist_id}")
+      .body(StringBody(newArtist()))
       .asJSON
       .check(status.is(201))
   )
+
+  var listArtists = exec(
+    http("List artists")
+      .get("/artists")
+      .asJSON
+      .check(status.is(200))
+      .check(jsonPath("$..id").ofType[Int].findAll.saveAs("ids"))
+  )
+
+
+  val deleteAllArtists = scenario("Delete all artists")
+  .forever {
+    exec(listArtists)
+    .foreach("${ids}", "id") {
+      exec(
+        http("Delete artist")
+        .delete("/artists/${id}")
+        .check(status.is(200))
+      )
+      .pause(2)
+    }
+  }
 
   val singleUserScenario = scenario("Single user")
     .feed(data)
     .forever {
       feed(data)
       .pause(1)
-      .exec(http("discovery").options("/").headers(commonHeaders))
-      .exec(createWebsite)
+      //.exec(http("discovery").options("/").headers(commonHeaders))
+      .exec(createArtist)
+      .exec(updateArtist)
   }
 
   setUp(
     singleUserScenario.inject(
       rampUsers(1) over (2 seconds)
     )
+//    deleteAllArtists.inject(
+//      rampUsers(1) over (2 seconds)
+//    )
   ).protocols(httpConf)
-    .maxDuration(testDurationSec() seconds)
+    .maxDuration(600 seconds)
 
 }
