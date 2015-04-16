ed85b3a164f58f00a902db29fe80e6bd51c2cd6d
adebski <andrzej.debski91@gmail.com>
2015-04-16 19:49:51 +0200

    Fixed base url

--- src/test/scala/simulations/Common.scala
+++ src/test/scala/simulations/Common.scala
@@ -8,14 +8,13 @@ object Common {
 
   val httpConf = http
     // .proxy(Proxy("127.0.0.1", 8888).httpsPort(8888))
-    .baseURL("https://workshop.uxguards.com/whipping-boy/api/albums")
+    .baseURL("https://workshop.uxguards.com/whipping-boy/api/")
     .disableCaching
 
   val commonHeaders = Map(
     "User-Agent" -> "gatling/simulation",
     HttpHeaderNames.Accept -> HttpHeaderValues.ApplicationJson
   )
-
 
   def testDurationSec() = sys.props.get("durationSec").getOrElse("10").toDouble
   def maxUsers():Int = sys.props.get("maxUsers").getOrElse("1").toInt
